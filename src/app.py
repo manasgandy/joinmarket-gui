00192f3a972e09a7351cee185b34167e33d40dbf
Manas Gandy <manasgandy@protonmail.com>
2021-12-10 10:01:16 -0500

    IP bug fix

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ import qrcode
 app = Flask(__name__)
 app.secret_key = b'joinmarket-gui'
 
-API_IP = "10.0.0.4"
+API_IP = "localhost"
 API_PORT = 28183
 API_URL = "https://" + API_IP + ":" + str(API_PORT) + "/api/v1"
 
